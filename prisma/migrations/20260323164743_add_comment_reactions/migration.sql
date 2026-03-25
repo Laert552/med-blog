50876b9382be7a4f4f7586e24eff3143d4c9ed4a
Laert <ghambaryanlaert1@gmail.com>
2026-03-25 18:31:15 +0400

    arden chem dzgum

--- prisma/migrations/20260323164743_add_comment_reactions/migration.sql
+++ prisma/migrations/20260323164743_add_comment_reactions/migration.sql
@@ -1,13 +1,20 @@
+-- CreateEnum
+CREATE TYPE "ReactionType" AS ENUM ('LIKE', 'DISLIKE');
+
 -- CreateTable
 CREATE TABLE "CommentReaction" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "commentId" TEXT NOT NULL,
     "voterId" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    CONSTRAINT "CommentReaction_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "Comment" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    "type" "ReactionType" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "CommentReaction_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "CommentReaction_commentId_voterId_key" ON "CommentReaction"("commentId", "voterId");
+
+-- AddForeignKey
+ALTER TABLE "CommentReaction" ADD CONSTRAINT "CommentReaction_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "Comment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
